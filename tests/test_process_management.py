e5013b4784531808c7a4cfdcc53cbbd8917f858b
Baris Metin <bmetin@yelp.com>
2012-11-29 15:45:34 -0800

    test that we can fork the child (to be traced)

--- tests/test_process_management.py
+++ tests/test_process_management.py
@@ -1,8 +1,21 @@
+import os
+
 import catbox
 import testify as T
 
 class ProcessManagement(T.TestCase):
 
-	def test_dummy(self):
-		assert False
+	@T.setup
+	def prepare(self):
+		self.MAX_READ_SIZE = 1024
+		self.read_pipe, self.write_pipe = os.pipe()
+		self.expected_message_from_child = "I'm ALIVE!"
+		def __child_function():
+			os.write(self.write_pipe, self.expected_message_from_child)
 
+		self.child_function = __child_function
+
+	def test_child(self):
+		catbox.run(self.child_function)
+		actual_message_from_child = os.read(self.read_pipe, self.MAX_READ_SIZE)
+		T.assert_equal(actual_message_from_child, self.expected_message_from_child)
