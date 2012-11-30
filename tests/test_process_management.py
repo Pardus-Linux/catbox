8b9ae87ca33b6235dd1f06f9a387c3b0308cf342
Baris Metin <bmetin@yelp.com>
2012-11-29 16:07:36 -0800

    document test method. Use select.epoll to timeout on read

--- tests/test_process_management.py
+++ tests/test_process_management.py
@@ -1,4 +1,5 @@
 import os
+import select
 
 import catbox
 import testify as T
@@ -6,16 +7,33 @@ import testify as T
 class ProcessManagement(T.TestCase):
 
 	@T.setup
-	def prepare(self):
+	def prepare_parent_child_communication(self):
 		self.MAX_READ_SIZE = 1024
+		# Create a pipe to verify that child is created and can
+		# communicate with the parent.
 		self.read_pipe, self.write_pipe = os.pipe()
 		self.expected_message_from_child = "I'm ALIVE!"
+
+		self.epoll = select.epoll()
+		self.epoll.register(self.read_pipe, select.EPOLLIN | select.EPOLLET)
+
 		def __child_function():
 			os.write(self.write_pipe, self.expected_message_from_child)
-
 		self.child_function = __child_function
 
 	def test_child(self):
+		"""catbox.run will fork(). Child process will execute
+		self.child_function. This test verifies that child is actually
+		running the given function reporting back to parent through a
+		Unix pipe.
+		"""
 		catbox.run(self.child_function)
-		actual_message_from_child = os.read(self.read_pipe, self.MAX_READ_SIZE)
-		T.assert_equal(actual_message_from_child, self.expected_message_from_child)
+
+		events = self.epoll.poll(.1)
+		if events:
+			first_event = events[0]
+			read_fd = first_event[0]
+			actual_message_from_child = os.read(read_fd, self.MAX_READ_SIZE)
+			T.assert_equal(actual_message_from_child, self.expected_message_from_child)
+		else:
+			assert False, "Child did not report back!"
